8029bf6b9ad5ca48c4643e6cdcdec97df1feb14f
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-02-20 20:49:00 +0100

    Introduction to Arrays
    Introduction to array and learning how to use the following arrays [push, slice, indexOf, .length].

--- Data Structures/Basics Of Arrays/note1.js
+++ Data Structures/Basics Of Arrays/note1.js
@@ -2,7 +2,42 @@
  * What are arrays?
  * Arrays are basically used to store multiply values inside them
  * 
+ * By the end of this course you should be able to list different
+ * types of array method.
+ * 
  */
+// Array Methods [push, slice, indexOf, .length]
 
 const groceries = ['apples', 'mangoes', 'pear', 'banana'];
-console.log("Benji");
+console.log(groceries);
+// to access item pear
+// this process is called indexing
+console.log(groceries[2]);
+console.log(groceries[3]);
+
+/** 
+ * push() Method
+ * This adds one or more elements to the end of an array and returns
+ * the new length of the array.
+ * */
+groceries.push('cookies', 'grape');
+console.log(groceries[5]);
+
+/** 
+ * slice() Method
+ * This method returns the portion of the array you want in a new array.
+ * */
+console.log(groceries.slice(0,2));// counts only 0 & 1, leaves 2
+
+/** 
+ * indexOf() Method
+ * This is use to locate or find the index of a value in an array.
+ * */
+console.log(groceries.indexOf('pear'));
+
+/** 
+ * .length() Method
+ * Length is an array property that returns the number of elements
+ * you have in a given array.
+ * */
+console.log(groceries.length);
