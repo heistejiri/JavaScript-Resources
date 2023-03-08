b433f9d74dee2b2703cec4ea1c715c6e675c00d2
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-08 17:33:34 +0100

    Update script.js

---
+++ Intro to JavaScript/script.js
@@ -0,0 +1,7 @@
+var name = 'Tejiri'
+/* Difference btw Var, Let and Const is that;
+Var can be used through out the whole program.
+Let can only be used within the scope of where you declared it.
+Const is a variable that can never change.
+*/
+console.log(name);
