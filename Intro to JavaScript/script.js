67b9e53f8d9f34966d80cf2acea6c3003836d4ac
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-14 12:12:21 +0100

    Updated

--- Intro to JavaScript/script.js
+++ Intro to JavaScript/script.js
@@ -5,3 +5,8 @@ Let can only be used within the scope of where you declared it.
 Const is a variable that can never change.
 */
 console.log(name);
+
+var myName = 'Tejiri';
+var myFullName = 'My fullname is ' + myName + ' Agoro';
+
+console.log(myFullName);
